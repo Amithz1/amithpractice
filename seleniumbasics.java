1387cdfa05b4bba5b48290ab315b4f435aab23c1
unknown <amithmurukesh981@gmail.com>
2025-02-10 13:00:43 +0530

    secondd

---
+++ seleniumbasics.java
@@ -0,0 +1,14 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+
+public class seleniumbasics {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		driver = new Chromdriver();
+		
+
+	}
+
+}
